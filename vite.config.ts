f448bf4ac1c361798294290d770869c00e2df5f2
Naveh Elya <102157763+navelya@users.noreply.github.com>
2025-11-14 20:13:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
   plugins: [react(), tailwindcss()],
-  base: '/efrat-is-my-love/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  }
+  base: '/efrat-is-my-love/'
 })
